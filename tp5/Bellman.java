90656cf67e19f6d62fb7082b26cb8b643a87f38c
LouisPopovic <louis.popovic@polymtl.ca>
2018-11-19 16:48:25 -0500

    ajout tp5

---
+++ tp5/Bellman.java
@@ -0,0 +1,59 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Stack;
+import java.util.Vector;
+
+import javax.lang.model.util.ElementScanner6;
+
+import javafx.scene.Node;
+
+
+public class Bellman {
+	private Graph graph;
+	private Node sourceNode;
+	private List<Vector<Double>> piTable =  new ArrayList<Vector<Double>>();
+	private List<Vector<Integer>> rTable =  new ArrayList<Vector<Integer>>();
+	
+	static final Integer inf = 99999;
+	
+	public Bellman (Graph g) {
+		this.graph = g;
+	}
+	
+	public void setSourceNode(Node source) {
+		this.sourceNode = source;
+	}
+	
+	public void shortestPath() {
+		
+		for(Node n : graph.getNodes()){
+
+			piTable.add(new Vector<Double>());		//Ajoute un vecteur pour chaque noeud
+			
+			piTable.get(n.hashCode()).add( n.equals(sourceNode) ? 0.0 : (double)inf ); //Ajoute 0 au vecteur du sourceNode, et infini sinon
+
+			rTable.add(new Vector<Integer>());		// Ajoute un vecteur pour chaque noeud
+
+			rTable.get(n.hashCode()).add( inf );	// Ajoute infini au vecteur de chaque noeud
+		}
+
+		Integer k = 1;
+
+		for(;;){
+				
+		}
+
+		
+	}
+	
+	public void  diplayShortestPaths() {
+		//Completer
+	}
+
+	public void displayTables() {
+	 //Completer
+	}
+}
---
+++ tp5/Graph.java
@@ -0,0 +1,132 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+
+public class Graph {
+
+	private List<Node> nodes = new ArrayList<Node>(); // Noeuds
+	private List<Edge> edges = new ArrayList<Edge>();; // Les arcs
+	static final double inf = 99999;
+	public Graph() {
+		
+	}
+
+	public void readFromFile(String filePath,String separtor){
+		
+		BufferedReader br = null;
+		FileReader fr = null;
+
+		try {
+
+			fr = new FileReader(filePath);
+			br = new BufferedReader(fr);
+
+			String sCurrentLine;
+
+			sCurrentLine = br.readLine();
+			String[] noeuds = sCurrentLine.split(separtor);
+
+			Integer id = 0;
+
+			//Creation des noeuds
+			for(String node : noeuds){
+				nodes.add(new Node(id++, node));
+			}
+
+			id = 0;
+
+			while ((sCurrentLine = br.readLine()) != null) {
+				//Creer les arcs
+				String[] distances = sCurrentLine.split(separtor);
+
+				for(int i = 0; i < distances.length; i++){
+					String dist = distances[i];
+				
+					if(!dist.equals("inf"))
+						edges.add(new Edge(nodes.get(id), nodes.get(i), Double.parseDouble(dist)));
+				}
+
+				id++;
+				
+			}
+
+		} catch (IOException e) {
+
+			e.printStackTrace();
+
+		} finally {
+
+			try {
+
+				if (br != null)
+					br.close();
+
+				if (fr != null)
+					fr.close();
+
+			} catch (IOException ex) {
+
+				ex.printStackTrace();
+
+			}
+		}
+	}
+	
+	public List<Edge> getOutEdges(Node source) {
+		List<Edge> outEdges = new ArrayList<Edge>();
+		
+		for (Edge e : edges) {
+			if(e.getSource() == source)
+				outEdges.add(e);
+		}
+		
+		return outEdges;	
+	}
+	
+	public List<Edge> getInEdges(Node dest) {
+		List<Edge> inEdges = new ArrayList<Edge>(); 
+
+		for (Edge e : edges) {
+			if(e.getDestination() == dest)
+				inEdges.add(e);
+		}
+		return inEdges;		
+	}
+
+	// Accesseurs 
+	public List<Node> getNodes() {
+		return nodes;
+	}
+	public void setNodes(List<Node> nodes) {
+		this.nodes = nodes;
+	}
+	public List<Edge> getEdges() {
+		return edges;
+	}
+	public void setEdges(List<Edge> edges) {
+		this.edges = edges;
+	}
+	public Node getNodeByName(String name){
+		for (Node node : nodes) {
+			if(node.getName().equals(name)){
+				return node;
+			}
+		}
+		return null;
+	}
+	
+	public Node getNodeById(int id){
+		for (Node node : nodes) {
+			if(node.getId()==id){
+				return node;
+			}
+
+		}
+		return null;
+	}
+}
