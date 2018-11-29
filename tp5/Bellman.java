58109587633a5d7cca560e519466011cf7fe840f
Louis Popovic <louispop@gmail.com>
2018-11-29 13:02:47 -0500

    algo bellman

--- tp5/Bellman.java
+++ tp5/Bellman.java
@@ -6,9 +6,6 @@ import java.util.Map;
 import java.util.Stack;
 import java.util.Vector;
 
-import javax.lang.model.util.ElementScanner6;
-
-import javafx.scene.Node;
 
 
 public class Bellman {
@@ -29,32 +26,76 @@ public class Bellman {
 	
 	public void shortestPath() {
 		
-		for(Node n : graph.getNodes()){	
-
-			piTable.add(new Vector<Double>());		//Ajoute un vecteur pour chaque noeud
-			
-			piTable.get(n.hashCode()).add( n.equals(sourceNode) ? 0.0 : (double)inf ); //Ajoute 0 au vecteur du sourceNode, et infini sinon
-
-			rTable.add(new Vector<Integer>());		// Ajoute un vecteur pour chaque noeud
-
-			rTable.get(n.hashCode()).add( inf );	// Ajoute infini au vecteur de chaque noeud
-		}
-
+		//1. On ajoute le premier vecteur a notre piTable pour k = 0
+		//   Meme chose pour la rTable
+		piTable.add( new Vector<Double>());
+		rTable.add( new Vector<Integer>());
 		Integer k = 0;
+
+		//2. Initialiser les distances dans piTable a Infini pour tous les noeuds, sauf src = 0
+		//   Mettre Infini dans la rTable pour tout les noeuds
+		for (Node n : this.graph.getNodes()){
+			piTable.get(k).add( n.equals(this.sourceNode) ? 0.0 : (double)inf );
+			rTable.get(k).add(inf);
+		}
+		
 
 		for(;;){
 			
-			k += 1;
+			// On incremente k et on insere un nouveau vecteur dans notre piTable et rTable
+			k++;
+			piTable.add(new Vector<Double>());
+			rTable.add( new Vector<Integer>());
 
-			List<Vector<Double>> piKMinusOne = piTable[k - 1];
+			for(Node n : this.graph.getNodes()){
 
-			for (Node node : graph.getNodes()) { 
-				Double kMinusOneValue = 
-			}
+				//On copie le piTable -1 a k et le rTable
+				piTable.set(k, piTable.get(k-1));
+				rTable.set(k, rTable.get(k-1));
+
+				// PI^k-1(n) : temps Minimal jusqu'a present pour se rendre au Node n
+				Double currentMinimalTimeToNode = piTable.get(k - 1).get(n.hashCode());
 				
-		}
+				// On trouve tous les arcs rentrants dans le Node n
+				List<Edge> inEdges =  this.graph.getInEdges(n);
+				
+				// On cherche le temps minimum pour se rendre a n a partir d'un de ses predecesseurs
+				// On prend le minimum entre le PI^k-1(n) et le plus rapide a partir de ses predecesseurs
 
-		
+				// On met minWeightToN a inf
+				Double minValue = (double)inf;
+				Node previousNodeToN = rTable.get(k - 1).get(n.hashCode);
+				
+				for ( Edge e : inEdges){
+					// On sait que la destination est n
+					Node previousNode = e.getSource();
+					
+					//Le temps minimal jusqua present pour se rendre a son predecesseur
+					Double currentWeightToPredecessor = piTable.get(k-1).get(previousNode.hashCode());
+					Double weightFromPredecessorToN = e.getDistance();
+					
+					if((currentWeightToPredecessor + weightFromPredecessorToN) < minValue){
+						minValue = currentMinimalTimeToNode + weightFromPredecessorToN;
+						previousNodeToN = previousNode;
+					}
+					
+				}
+				
+				// Si le weight a partir d'un predecesseur est plus rapide que le temps courant, on change le temps courant
+				if(minValue < currentMinimalTimeToNode){
+					piTable.get(k).set(n.hashCode(), minValue);
+					//Et on met a jour la rTable
+					rTable.get(k).set(n.hashCode(), previousNodeToN.hashCode());
+				}
+			}
+
+			// Si pi^k == pi^k-1 on arrete, il n'y a eu aucune modification
+			if(piTable.get(k-1) == piTable.get(k)){
+				break;
+			}
+
+			//verifier si negatif
+		}
 	}
 	
 	public void  diplayShortestPaths() {
--- tp5/Main.java
+++ tp5/Main.java
@@ -14,12 +14,12 @@ public class Main {
 		g.readFromFile("./graphe.txt",",");
 
 		// Exerice 2: calcul du plus court chemin de source vers tous les sommet
-		/*Bellman bl = new Bellman(g);
+		Bellman bl = new Bellman(g);
 		Node source=g.getNodeByName("S");
 		bl.setSourceNode(source);
 		bl.shortestPath();
 		bl.displayTables();
 		// Exerice 3: affichage des plus courts chemins
-		bl.diplayShortestPaths();	*/
+		//bl.diplayShortestPaths();	*/
 	}
 }
